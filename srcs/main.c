0f5a65614e390dd48a242eaf07266e5e671d819f
tristeceratops <eric.woillard@gmail.com>
2026-05-13 16:55:36 +0200

    small test

--- srcs/main.c
+++ srcs/main.c
@@ -1,40 +1,67 @@
 #include "woody_woodpacker.h"
 #include "utils.h"
 
+int isElf64(int fd){
+	char fileHeader[5];
+	char elf64MagicBytes[6] = "\x7f\x45\x4c\x46\x02\x00";
+
+	
+	read(fd, fileHeader, 5);
+	
+	if (strncmp(fileHeader, elf64MagicBytes, 5) == 0)
+		return 1;
+	else
+		return 0;
+}
+
 int main(void)
 {
-	const char *path = "example.txt";
-	const char *message = "Hello from mmap!\nThis file was created and written using mmap.\n";
-	const size_t len = strlen(message);
 	int fd;
-	void *map;
+	off_t old_size;
+	off_t new_size;
 
-	fd = open(path, O_RDWR | O_CREAT | O_TRUNC, 0644);
-	if (fd < 0)
-	{
+	char payload[] = "WOODY";
+
+	fd = open("test/hello", O_RDWR);
+	if (fd < 0){
 		perror("open");
-		return (1);
+		return 1;
 	}
-	if (ftruncate(fd, (off_t)len) == -1)
-	{
-		perror("ftruncate");
-		close(fd);
-		return (1);
+
+	if (!isElf64(fd)){
+		perror("not elf 64");
+		return 1;
 	}
-	map = mmap(NULL, len, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-	if (map == MAP_FAILED)
-	{
+
+	old_size = lseek(fd, 0, SEEK_END);
+	printf("Old size: %ld\n", old_size);
+	new_size = old_size + sizeof(payload);
+
+	lseek(fd, new_size - 1, SEEK_SET);
+	write(fd, "\0", 1);
+
+	char *mapped = mmap(
+		NULL,
+		new_size,
+		PROT_READ | PROT_WRITE,
+		MAP_SHARED,
+		fd,
+		0);
+	
+	if (mapped == MAP_FAILED){
 		perror("mmap");
-		close(fd);
-		return (1);
+		return 1;
 	}
-	ft_memcpy(map, message, len);
-	if (msync(map, len, MS_SYNC) == -1)
-		perror("msync");
-	if (munmap(map, len) == -1)
-		perror("munmap");
-	if (close(fd) == -1)
-		perror("close");
-	printf("Wrote %zu bytes to %s using mmap.\n", len, path);
-	return (0);
+
+	for (size_t i = 0; i < sizeof(payload); i++)
+		mapped[old_size + i] = payload[i];
+	
+	msync(mapped, new_size, MS_SYNC);
+
+	munmap(mapped, new_size);
+	close(fd);
+
+	printf("Payload appended.\n");
+
+	return 0;
 }
---
+++ test/hello_world.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+//cc -no-pie -c hello_world.c
+//cc hello_world.o -o hello
+
+int main()
+{
+    printf("Hello world!");   
+}
