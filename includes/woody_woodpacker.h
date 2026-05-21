9fceb3a5a8d0587b51339a5b803ffbb949098d2b
tristeceratops <eric.woillard@gmail.com>
2026-05-21 18:51:40 +0200

    parsing headers finish

--- includes/woody_woodpacker.h
+++ includes/woody_woodpacker.h
@@ -1,14 +1,30 @@
 #ifndef WOODY_WOODPACKER_H
-# define WOODY_WOODPACKER_H
+#define WOODY_WOODPACKER_H
 
-    # include <errno.h>
-    # include <fcntl.h>
-    # include <stdio.h>
-    # include <stdlib.h>
-    # include <string.h>
-    # include <sys/mman.h>
-    # include <sys/stat.h>
-    # include <sys/types.h>
-    # include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <elf.h>
+
+typedef struct s_elf64_section_headers
+{
+    Elf64_Shdr shdr;
+    char *name_str_format;
+} t_elf64_section_headers;
+
+// custom structures to store all informations of ELF header, program headers and section headers
+typedef struct s_elf64_headers
+{
+    Elf64_Ehdr ehdr;
+    Elf64_Phdr *phdr;
+    t_elf64_section_headers *shdr;
+} t_elf64_headers;
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -1,98 +1,244 @@
 #include "woody_woodpacker.h"
 #include "utils.h"
-#include <stdint.h>
-#include <elf.h>
 
-//e_entry is always at bytes 0x18=24 and it's 8 bytes longs (4 in ELF 32)
-Elf64_Ehdr readElfHeader(int fd)
+int isElf64_fd(int fd)
 {
-	Elf64_Ehdr ehdr;
+	unsigned char ident[EI_NIDENT];
 
-    read(fd, &ehdr, sizeof(ehdr));
-
-    return ehdr;
-}
-
-Elf64_Phdr readProgramdHeader(int fd, const Elf64_Ehdr *ehdr, size_t index)
-{
-    Elf64_Phdr phdr = {0};
-    off_t off = (off_t)ehdr->e_phoff + (off_t)(index * ehdr->e_phentsize);
-
-    if (lseek(fd, off, SEEK_SET) == (off_t)-1) {
-        perror("lseek program header");
-        return phdr;
-    }
-    if (read(fd, &phdr, sizeof(phdr)) != (ssize_t)sizeof(phdr)) {
-        perror("read program header");
-    }
-    return phdr;
-}
-
-int isElf64(int fd){
-	char fileHeader[5];
-	char elf64MagicBytes[6] = "\x7f\x45\x4c\x46\x02\x00";
-
-	
-	read(fd, fileHeader, 5);
-	
-	if (strncmp(fileHeader, elf64MagicBytes, 5) == 0)
-		return 1;
-	else
+	if (lseek(fd, 0, SEEK_SET) == (off_t)-1)
 		return 0;
+	if (read(fd, ident, EI_NIDENT) != EI_NIDENT)
+		return 0;
+	/* ELF magic + class 2 = 64-bit */
+	return (ident[EI_MAG0] == ELFMAG0 &&
+			ident[EI_MAG1] == ELFMAG1 &&
+			ident[EI_MAG2] == ELFMAG2 &&
+			ident[EI_MAG3] == ELFMAG3 &&
+			ident[EI_CLASS] == ELFCLASS64);
 }
 
-int main(void)
+void free_elf64_headers(t_elf64_headers *hdrs)
 {
-	int fd;
-	off_t old_size;
-	off_t new_size;
+	if (!hdrs)
+		return;
+	if (hdrs->phdr)
+		free(hdrs->phdr);
+	if (hdrs->shdr)
+	{
+		for (size_t i = 0; i < hdrs->ehdr.e_shnum; i++)
+			free(hdrs->shdr[i].name_str_format);
+		free(hdrs->shdr);
+	}
+	free(hdrs);
+}
 
-	char payload[] = "WOODY";
+int load_section_names(int fd, t_elf64_headers *hdrs)
+{
+	const Elf64_Shdr *shstr_hdr;
+	char *shstrtab;
 
-	fd = open("test/hello", O_RDWR);
-	if (fd < 0){
+	if (!hdrs || !hdrs->shdr || hdrs->ehdr.e_shstrndx >= hdrs->ehdr.e_shnum)
+		return 0;
+	shstr_hdr = &hdrs->shdr[hdrs->ehdr.e_shstrndx].shdr;
+	shstrtab = calloc(1, shstr_hdr->sh_size + 1);
+	if (!shstrtab)
+		return 0;
+	if (lseek(fd, (off_t)shstr_hdr->sh_offset, SEEK_SET) == (off_t)-1)
+	{
+		free(shstrtab);
+		return 0;
+	}
+	if (read(fd, shstrtab, shstr_hdr->sh_size) != (ssize_t)shstr_hdr->sh_size)
+	{
+		free(shstrtab);
+		return 0;
+	}
+	for (size_t i = 0; i < hdrs->ehdr.e_shnum; i++)
+	{
+		if (hdrs->shdr[i].shdr.sh_name < shstr_hdr->sh_size)
+			hdrs->shdr[i].name_str_format = strdup(shstrtab + hdrs->shdr[i].shdr.sh_name);
+		else
+			hdrs->shdr[i].name_str_format = strdup("");
+		if (!hdrs->shdr[i].name_str_format)
+		{
+			free(shstrtab);
+			return 0;
+		}
+	}
+	free(shstrtab);
+	return 1;
+}
+
+t_elf64_headers *load_elf64_headers(int fd)
+{
+	t_elf64_headers *hdrs = NULL;
+	Elf64_Ehdr ehdr;
+	ssize_t r;
+	off_t off;
+
+	if (lseek(fd, 0, SEEK_SET) == (off_t)-1)
+		return NULL;
+	r = read(fd, &ehdr, sizeof(ehdr));
+	if (r != (ssize_t)sizeof(ehdr))
+		return NULL;
+
+	hdrs = calloc(1, sizeof(*hdrs));
+	if (!hdrs)
+		return NULL;
+	hdrs->ehdr = ehdr;
+
+	if (ehdr.e_phnum > 0)
+	{
+		hdrs->phdr = calloc(ehdr.e_phnum, sizeof(Elf64_Phdr));
+		if (!hdrs->phdr)
+		{
+			free_elf64_headers(hdrs);
+			return NULL;
+		}
+		for (size_t i = 0; i < ehdr.e_phnum; i++)
+		{
+			off = (off_t)ehdr.e_phoff + (off_t)(i * ehdr.e_phentsize);
+			if (lseek(fd, off, SEEK_SET) == (off_t)-1)
+			{
+				free_elf64_headers(hdrs);
+				return NULL;
+			}
+			if (read(fd, &hdrs->phdr[i], sizeof(Elf64_Phdr)) != (ssize_t)sizeof(Elf64_Phdr))
+			{
+				free_elf64_headers(hdrs);
+				return NULL;
+			}
+		}
+	}
+
+	if (ehdr.e_shnum > 0)
+	{
+		hdrs->shdr = calloc(ehdr.e_shnum, sizeof(t_elf64_section_headers));
+		if (!hdrs->shdr)
+		{
+			free_elf64_headers(hdrs);
+			return NULL;
+		}
+		for (size_t i = 0; i < ehdr.e_shnum; i++)
+		{
+			off = (off_t)ehdr.e_shoff + (off_t)(i * ehdr.e_shentsize);
+			if (lseek(fd, off, SEEK_SET) == (off_t)-1)
+			{
+				free_elf64_headers(hdrs);
+				return NULL;
+			}
+			if (read(fd, &hdrs->shdr[i], sizeof(Elf64_Shdr)) != (ssize_t)sizeof(Elf64_Shdr))
+			{
+				free_elf64_headers(hdrs);
+				return NULL;
+			}
+		}
+		if (!load_section_names(fd, hdrs))
+		{
+			free_elf64_headers(hdrs);
+			return NULL;
+		}
+	}
+
+	return hdrs;
+}
+
+void print_e_ident(const unsigned char ident[EI_NIDENT])
+{
+	printf("  e_ident: ");
+	for (int i = 0; i < EI_NIDENT; ++i)
+		printf("%02x ", ident[i]);
+	printf("\n");
+}
+
+void print_elf64_headers(const t_elf64_headers *h)
+{
+	if (!h)
+		return;
+	const Elf64_Ehdr *e = &h->ehdr;
+	print_e_ident(e->e_ident);
+	printf("ELF Header:\n");
+	printf("  e_type:      0x%x\n", e->e_type);
+	printf("  e_machine:   0x%x\n", e->e_machine);
+	printf("  e_version:   0x%x\n", e->e_version);
+	printf("  e_entry:     0x%lx\n", (unsigned long)e->e_entry);
+	printf("  e_phoff:     %lu\n", (unsigned long)e->e_phoff);
+	printf("  e_shoff:     %lu\n", (unsigned long)e->e_shoff);
+	printf("  e_flags:     0x%x\n", e->e_flags);
+	printf("  e_ehsize:    %u\n", e->e_ehsize);
+	printf("  e_phentsize: %u\n", e->e_phentsize);
+	printf("  e_phnum:     %u\n", e->e_phnum);
+	printf("  e_shentsize: %u\n", e->e_shentsize);
+	printf("  e_shnum:     %u\n", e->e_shnum);
+	printf("  e_shstrndx:  %u\n", e->e_shstrndx);
+
+	if (h->phdr && e->e_phnum)
+	{
+		printf("\nProgram Headers (%u):\n", e->e_phnum);
+		for (size_t i = 0; i < e->e_phnum; ++i)
+		{
+			const Elf64_Phdr *p = &h->phdr[i];
+			printf(" PHDR %zu:\n", i);
+			printf("  p_type:   0x%x\n", p->p_type);
+			printf("  p_flags:  0x%x\n", p->p_flags);
+			printf("  p_offset: %lu\n", (unsigned long)p->p_offset);
+			printf("  p_vaddr:  0x%lx\n", (unsigned long)p->p_vaddr);
+			printf("  p_paddr:  0x%lx\n", (unsigned long)p->p_paddr);
+			printf("  p_filesz: %lu\n", (unsigned long)p->p_filesz);
+			printf("  p_memsz:  %lu\n", (unsigned long)p->p_memsz);
+			printf("  p_align:  %lu\n", (unsigned long)p->p_align);
+		}
+	}
+
+	if (h->shdr && e->e_shnum)
+	{
+		printf("\nSection Headers (%u):\n", e->e_shnum);
+		for (size_t i = 0; i < e->e_shnum; ++i)
+		{
+			const Elf64_Shdr *s = &h->shdr[i].shdr;
+			printf(" SHDR %zu:\n", i);
+			printf("  sh_name:      %u/%s\n", s->sh_name, h->shdr[i].name_str_format);
+			printf("  sh_type:      0x%x\n", s->sh_type);
+			printf("  sh_flags:     0x%lx\n", (unsigned long)s->sh_flags);
+			printf("  sh_addr:      0x%lx\n", (unsigned long)s->sh_addr);
+			printf("  sh_offset:    %lu\n", (unsigned long)s->sh_offset);
+			printf("  sh_size:      %lu\n", (unsigned long)s->sh_size);
+			printf("  sh_link:      %u\n", s->sh_link);
+			printf("  sh_info:      %u\n", s->sh_info);
+			printf("  sh_addralign: %lu\n", (unsigned long)s->sh_addralign);
+			printf("  sh_entsize:   %lu\n", (unsigned long)s->sh_entsize);
+		}
+	}
+}
+
+int main(int argc, char **argv)
+{
+	const char *path = (argc > 1) ? argv[1] : "test/hello";
+	int fd = open(path, O_RDONLY);
+	if (fd < 0)
+	{
 		perror("open");
 		return 1;
 	}
 
-	if (!isElf64(fd)){
-		perror("not elf 64");
-		return 1;
-	}
-    lseek(fd, 0, SEEK_SET);
-	Elf64_Ehdr ehdr = readElfHeader(fd);
-    Elf64_Phdr phdr = readProgramdHeader(fd, &ehdr, 0);
-
-	printf("raw entry struct = 0x%lx\n", ehdr.e_entry);
-    printf("phdr[0].p_type = %u\n", phdr.p_type);
-
-	old_size = lseek(fd, 0, SEEK_END);
-	printf("Old size: %ld\n", old_size);
-	new_size = old_size + sizeof(payload);
-
-	lseek(fd, new_size - 1, SEEK_SET);
-	write(fd, "\0", 1);
-
-	char *mapped = mmap(
-		NULL,
-		new_size,
-		PROT_READ | PROT_WRITE,
-		MAP_SHARED,
-		fd,
-		0);
-	
-	if (mapped == MAP_FAILED){
-		perror("mmap");
+	if (!isElf64_fd(fd))
+	{
+		fprintf(stderr, "not an ELF64 file: %s\n", path);
+		close(fd);
 		return 1;
 	}
 
-	for (size_t i = 0; i < sizeof(payload); i++)
-		mapped[old_size + i] = payload[i];
-	
-	munmap(mapped, new_size);
+	t_elf64_headers *hdrs = load_elf64_headers(fd);
+	if (!hdrs)
+	{
+		fprintf(stderr, "failed to load ELF headers\n");
+		close(fd);
+		return 1;
+	}
+
+	print_elf64_headers(hdrs);
+
+	free_elf64_headers(hdrs);
 	close(fd);
-
-	printf("Payload appended.\n");
-
 	return 0;
 }
